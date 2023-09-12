74dc5d032698e00a2f6b63b4d6726d9b04d43421
Lizz <lizzcklein@gmail.com>
2023-09-11 22:50:10 -0400

    tests passed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+return "This Saturday, I want to " + activity + "!";
+}
+
+const mondayWork = function (task = "go to the office") {
+    return "This Monday, I will " + task + ".";
+}
+
+function wrapAdjective(visualFlair = "*") {
+   return (function(thing = "special"){
+    return "You are " + visualFlair + thing + visualFlair + "!";
+    });
+}
+
+
+
